1732a43bc499feb8c1b7d34e8168b0c035ea664f
root <root@LAPTOP-F86AFOMK.localdomain>
2021-11-30 22:57:38 +0800

    event.stopPropagation

--- pr_1130/event.js
+++ pr_1130/event.js
@@ -1,31 +1,36 @@
+const wrap_div = document.querySelector(".container");
 const out_div = document.querySelector(".box");
 const in_div = document.querySelector(".item");
 
 
-out_div.addEventListener("click",()=>
-  {console.log("out clicked cap");
-},
-  true  
-);
+// out_div.addEventListener("click",()=>
+//   {console.log("out clicked cap");
+// },
+//   true  
+// );
 
-in_div.addEventListener("click",()=>
-  {console.log("in clicked bubble");
-},
+// in_div.addEventListener("click",()=>
+//   {console.log("in clicked bubble");
+// },
   
-);
+// );
 
-out_div.addEventListener("click",()=>
-  {console.log("out clicked bubble");
-},
+// out_div.addEventListener("click",()=>
+//   {console.log("out clicked bubble");
+// },
     
-);
+// );
+wrap_div.addEventListener("click", (e)=> {
+  e.stopPropagation();
+  console.log("out"); 
+});
 
 
-// out_div.addEventListener("click", (e)=> {
-//   e.stopPropagation();
-//   console.log("out"); 
-// });
+out_div.addEventListener("click", (e)=> {
+  e.stopPropagation();
+  console.log("out"); 
+});
 
-// in_div.addEventListener("click", (e)=> {
-//   console.log("in");
-// });
+in_div.addEventListener("click", (e)=> {
+  console.log("in");
+});
